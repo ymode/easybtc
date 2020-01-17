d4447c4e3fae0b228308760be91455abfad802d8
Brendan <sunworth@gmail.com>
2020-01-17 22:17:27 +1100

    Update easybtc.py
    added GBP and EUR

--- easybtc.py
+++ easybtc.py
@@ -1,7 +1,9 @@
 #This library is “Powered by CoinDesk” with express permission from Coindesk
 #Please visit https://www.coindesk.com/price/bitcoin for the Coindesk web experience
+#
+#
 #easybtc is meant to be an uncomplicated and easy way to integrate realitime BTC price into your project
-#github
+#github https://github.com/ymode/easybtc
 
 import requests as _r
 
@@ -11,9 +13,11 @@ class Request:
         self.status_c = self.request.status_code
         self.json = self.request.json()
         self.price = self.json['bpi']['USD']['rate']
+        self.priceGBP = self.json['bpi']['GBP']['rate']
+        self.priceEUR = self.json['bpi']['EUR']['rate']
 
 class Bitcoin:
     def __init__(self):
         self.price = Request().price
-
-
+        self.priceGBP = Request().priceGBP
+        self.priceEUR = Request().priceEUR
